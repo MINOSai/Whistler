532cdf9971c8892cb392572d942623d5f634524b
Yaswant Narayan <yoyaswant@gmail.com>
2017-12-04 22:28:31 +0530

    Able to get contacts

--- app/src/main/java/com/minosai/whistler/ContactDetails.java
+++ app/src/main/java/com/minosai/whistler/ContactDetails.java
@@ -1,14 +1,19 @@
 package com.minosai.whistler;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -16,6 +21,7 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -33,6 +39,12 @@ public class ContactDetails extends AppCompatActivity {
     ConstraintLayout constraintLayout;
     FloatingActionButton fab;
 
+    private static final String TAG = ContactDetails.class.getSimpleName();
+    private static final int REQUEST_CODE_PICK_CONTACTS = 1;
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 2;
+    private Uri uriContact;
+    private String contactID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +52,7 @@ public class ContactDetails extends AppCompatActivity {
 
         constraintLayout = (ConstraintLayout)findViewById(R.id.contactDetailsConstraint);
 
-//        recyclerView = (RecyclerView)findViewById(R.id.emergencyContactList);
+        recyclerView = (RecyclerView)findViewById(R.id.emergencyContactList);
 //        adapter = new EmergencyContactAdapter(names,numbers);
 //
 //        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
@@ -54,37 +66,94 @@ public class ContactDetails extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ContactDetails.this, "asdfasf", Toast.LENGTH_SHORT).show();
-                pickContact();
+                if (ContextCompat.checkSelfPermission(ContactDetails.this,
+                        Manifest.permission.READ_CONTACTS)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(ContactDetails.this,
+                            new String[]{Manifest.permission.READ_CONTACTS},
+                            PERMISSIONS_REQUEST_READ_CONTACTS);
+                }else {
+                    onClickSelectContact();
+                }
             }
         });
     }
 
-    private void pickContact() {
-        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);  //should filter only contacts with phone numbers
-        startActivityForResult(intent, PICK_CONTACT);
+    public void onClickSelectContact(){
+        startActivityForResult(new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI), REQUEST_CODE_PICK_CONTACTS);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (PICK_CONTACT != requestCode || RESULT_OK != resultCode) return;
-        Uri contactUri = data.getData();
-        String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
-        if (null == contactUri) return;
-        //no tampering with Uri makes this to work without READ_CONTACTS permission
-        Cursor cursor = getContentResolver().query(contactUri, projection, null, null, null);
-        if (null == cursor) return;
-        try {
-            while (cursor.moveToNext()) {
-                String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NAME_RAW_CONTACT_ID));
-                // ... use "number" as you wish
-                Toast.makeText(this, "number: "+number, Toast.LENGTH_SHORT).show();
-            }
-        } finally {
-            cursor.close();
+
+        if(requestCode == REQUEST_CODE_PICK_CONTACTS && resultCode == RESULT_OK){
+            Log.d(TAG, "Response: " + data.toString());
+            uriContact = data.getData();
+
+            retrieveContactName();
+            retrieveContactNumber();
+//            retrieveContactPhoto();
         }
-        // "cursor" is closed here already
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    onClickSelectContact();
+                }else{
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+            default: {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+    }
+
+    private void retrieveContactNumber() {
+
+        String contactNumber = null;
+
+        Cursor cursorID = getContentResolver().query(uriContact,
+                new String[]{ContactsContract.Contacts._ID},
+                null, null, null);
+        if (cursorID.moveToFirst()) {
+
+            contactID = cursorID.getString(cursorID.getColumnIndex(ContactsContract.Contacts._ID));
+        }
+        cursorID.close();
+
+        Log.d(TAG, "Contact ID: " + contactID);
+        Cursor cursorPhone = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " +
+                        ContactsContract.CommonDataKinds.Phone.TYPE + " = " +
+                        ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE,
+
+                new String[]{contactID},
+                null);
+        if (cursorPhone.moveToFirst()) {
+            contactNumber = cursorPhone.getString(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+        }
+        cursorPhone.close();
+        Log.d(TAG, "Contact Phone Number: " + contactNumber);
+    }
+
+    private void retrieveContactName() {
+
+        String contactName = null;
+        Cursor cursor = getContentResolver().query(uriContact, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        }
+        cursor.close();
+        Log.d(TAG, "Contact Name: " + contactName);
+
     }
 }
---
+++ app/src/main/java/com/minosai/whistler/contacts/Contact.java
@@ -0,0 +1,8 @@
+package com.minosai.whistler.contacts;
+
+/**
+ * Created by minos.ai on 04/12/17.
+ */
+
+public class Contact {
+}
